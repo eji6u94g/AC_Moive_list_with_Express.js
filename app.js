ad6accdc685672625678eb3d68c110899c912f73
eji6u94g <seanmybear@gmail.com>
2021-07-03 19:27:01 +0800

    1. Include static files(bootstraps, jquery, popper.js). 2. Render page using iteration {{#each Arr}}. 3. import JSON file for movie information array. 4. Using params to record movie.id from URL and use this recorded movie.id to filter movie info in JSON file. And render show page with this info. 5. Through req.query to get submit input value(querystring) to filter movies.

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express')
+// import handlebars
 const exphbs = require('express-handlebars')
+// import movie info from outter resource
+const movies = require('./movieList.json')
 const app = express()
 const port = 3000
 
+// setting express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { movies: movies.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const showMovie = movies.results.filter(item => item.id.toString() === req.params.movie_id)[0]
+  console.log(showMovie.id)
+  res.render('show', { movies: showMovie })
+})
+
+// search function
+app.get('/search', (req, res) => {
+  const filteredmovies = movies.results.filter(item => item.title.toLowerCase().includes(req.query.keywords.toLowerCase()))
+  res.render('index', { movies: filteredmovies, keyword: req.query.keywords })
 })
 
 app.listen(port, () => {
